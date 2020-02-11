ce59ca08cd3c2a3a66ee34cbe6e990536eab6632
williamwjd <william.jose.wjd@gmail.com>
2020-02-11 15:37:50 -0300

    Subindo servidor e testando rotas

---
+++ backend/src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const routes = require('./routes')
+
+require('dotenv/config')
+
+class App{
+    constructor(){
+        this.server=express()
+        this.middleware()
+        this.routes()
+    }
+
+    middleware(){
+        this.server.use(express.json())
+    }
+
+    routes(){
+        this.server.use(routes)
+    }
+}
+
+module.exports=new App().server
---
+++ backend/src/server.js
@@ -0,0 +1,5 @@
+const App = require('./app')
+
+App.listen(process.env.PORT_SERVER,()=>{
+    console.log('Servidor online')
+})
