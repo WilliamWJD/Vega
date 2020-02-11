2e5b509f6d93f864ee81e1a4364b515371a5a2a1
williamwjd <william.jose.wjd@gmail.com>
2020-02-11 16:06:56 -0300

    Criando conexao com o mongodb

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
 
 require('dotenv/config')
 
 class App{
     constructor(){
         this.server=express()
+        this.database()
         this.middleware()
         this.routes()
     }
@@ -16,6 +18,13 @@ class App{
 
     routes(){
         this.server.use(routes)
+    }
+
+    database(){
+        mongoose.connect(`${process.env.MONGO_URL}`,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        })
     }
 }
 
